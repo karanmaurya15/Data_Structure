cfb1af88190484aa2cf637d8f791c1592d403474
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-07 22:58:14 +0530

    Create a simple Linked is Done

--- Data_Structure/Program.cs
+++ Data_Structure/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Data Structure Program");
+
+            LinkedList ln = new LinkedList();
+            ln.Add(56);
+            ln.Add(30);
+            ln.Add(70);
+            ln.Display();
         }
     }
 }
