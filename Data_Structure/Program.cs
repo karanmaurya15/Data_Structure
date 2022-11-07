a4ac80217fce26ee9e962df3fcf76eaa3eac2438
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-07 22:13:12 +0530

    Wellcome Program

---
+++ Data_Structure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Data_Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Data Structure Program");
+        }
+    }
+}
