93febcede57feccd83d8b34130be6e8fef18024d
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-08 17:07:55 +0530

    Delete Last Element in Linked List is Done

--- Data_Structure/Program.cs
+++ Data_Structure/Program.cs
@@ -10,7 +10,7 @@
             ln.Add(56);
             ln.Add(30);
             ln.Add(70);
-            ln.RemoveFirstNode();
+            ln.RemoveLastNode();
             Console.WriteLine("After removing is : ");
             ln.Display();
             
