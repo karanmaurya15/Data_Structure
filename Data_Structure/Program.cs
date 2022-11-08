3d6c0d5a5eaf5a0f20f752df752751d5f179a13b
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-08 16:43:06 +0530

    Appending in Linked List is Done

--- Data_Structure/Program.cs
+++ Data_Structure/Program.cs
@@ -7,9 +7,11 @@
             Console.WriteLine("Wellcome to Data Structure Program");
 
             LinkedList ln = new LinkedList();
-            ln.Add(70);
-            ln.Add(30);
             ln.Add(56);
+            ln.Add(30);
+            ln.Add(70);
+            ln.InsertAtParticularPosition(2, 30);
+            ln.Display();
             
         }
     }
