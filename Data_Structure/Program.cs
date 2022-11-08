f78e9a8139185609a7c500a1bb911800c2dbb96b
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-08 16:50:24 +0530

    Insert Between  Linked List is Done

--- Data_Structure/Program.cs
+++ Data_Structure/Program.cs
@@ -10,7 +10,7 @@
             ln.Add(56);
             ln.Add(30);
             ln.Add(70);
-            ln.InsertAtParticularPosition(2, 30);
+          
             ln.Display();
             
         }
